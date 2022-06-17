0a9259aecd6405bd5ec09b0f785d1e06ba5b35bb
itcongsang <stitcongsang@gmail.com>
2022-06-17 13:55:55 +0700

    update 17/06/2022

--- testapp/client/lib/tests/tests.dart
+++ testapp/client/lib/tests/tests.dart
@@ -21,14 +21,20 @@ class Tests {
     _items = List<Test>();
 
     _items.add(ClientMethodNoParametersNoReturnValue(connectionProvider, logger));
-    _items.add(ClientMethodNoParametersSimpleReturnValue(connectionProvider, logger));
-    _items.add(ClientMethodOneSimpleParameterNoReturnValue(connectionProvider, logger));
-    _items.add(ClientMethodOneSimpleParameterSimpleReturnValue(connectionProvider, logger));
-    _items.add(ClientMethodComplexParameterComplexReturnValue(connectionProvider, logger));
-
-    _items.add(ServerInvokeMethodNoParametersNoReturnValue(connectionProvider, logger));
-    _items.add(ServerInvokeMethodSimpleParametersNoReturnValue(connectionProvider, logger));
-
+    _items
+        .add(ClientMethodNoParametersNoReturnValue(connectionProvider, logger));
+    _items.add(
+        ClientMethodNoParametersSimpleReturnValue(connectionProvider, logger));
+    _items.add(ClientMethodOneSimpleParameterNoReturnValue(
+        connectionProvider, logger));
+    _items.add(ClientMethodOneSimpleParameterSimpleReturnValue(
+        connectionProvider, logger));
+    _items.add(ClientMethodComplexParameterComplexReturnValue(
+        connectionProvider, logger));
+    _items.add(ServerInvokeMethodNoParametersNoReturnValue(
+        connectionProvider, logger));
+    _items.add(ServerInvokeMethodSimpleParametersNoReturnValue(
+        connectionProvider, logger));
     _items.add(ClientInvokeStreamRequest(connectionProvider, logger));
   }
 }
